6b6ceda57428572ecd7ff497e7c80349f37c2d40
Cristiano Gomes <cristianno@ymail.com>
2018-09-08 00:11:42 -0300

    Atualizando endpoint da api e configuração do cors

--- src/app.js
+++ src/app.js
@@ -5,27 +5,16 @@ const cors = require('cors')
 const helmet = require('helmet')
 const config = require('./config/config')
 const { sequelize } = require('./models')
-const auth = require('./middleware/Auth')()
+const Auth = require('./middleware/Auth')()
 const ErrorHandler = require('./middleware/ErrorHandler')
 
 const app = express()
 
-var whitelist = [ 'http://localhost:3001', 'http://localhost:3000', 'http://127.0.0.1:3001', 'http://127.0.0.1:3000' ]
-var corsOptions = {
-  origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-
-app.use(cors(corsOptions))
+app.use(cors())
 app.use(helmet())
 app.use(morgan('combined'))
 app.use(bodyParser.json())
-app.use(auth.initialize())
+app.use(Auth.initialize())
 
 require('./routes')(app)
 
--- src/routes.js
+++ src/routes.js
@@ -5,15 +5,15 @@ const AsyncHandler = require('./middleware/AsyncHandler')
 const JoiValidator = require('./middleware/JoiValidator')
 
 module.exports = (app) => {
-  app.use('/task', Auth.authenticate())
+  app.use('/api/task', Auth.authenticate())
   app.use(JoiValidator())
 
-  app.get('/task', AsyncHandler(TaskController.list))
-  app.get('/task/:id', AsyncHandler(TaskController.get))
-  app.post('/task', AsyncHandler(TaskController.post))
-  app.put('/task/:id', AsyncHandler(TaskController.put))
-  app.delete('/task/:id', AsyncHandler(TaskController.delete))
+  app.get('/api/task', AsyncHandler(TaskController.list))
+  app.get('/api/task/:id', AsyncHandler(TaskController.get))
+  app.post('/api/task', AsyncHandler(TaskController.post))
+  app.put('/api/task/:id', AsyncHandler(TaskController.put))
+  app.delete('/api/task/:id', AsyncHandler(TaskController.delete))
 
-  app.post('/register', AsyncHandler(AuthController.register))
-  app.post('/login', AsyncHandler(AuthController.login))
+  app.post('/api/register', AsyncHandler(AuthController.register))
+  app.post('/api/login', AsyncHandler(AuthController.login))
 }
